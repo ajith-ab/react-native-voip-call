47cbde5f3fbd840f54ea52df13ba53888d2939a5
AndyHarrison146 <andyg146@googlemail.com>
2021-10-29 12:16:51 +0100

    fix for onEndCall method

--- android/src/main/java/com/ajith/voipcall/RNVoipNotificationHelper.java
+++ android/src/main/java/com/ajith/voipcall/RNVoipNotificationHelper.java
@@ -76,7 +76,7 @@ public class RNVoipNotificationHelper {
                 .setSound(sounduri)
                 .setContentText(json.getString("notificationBody"))
                 .addAction(0, json.getString("answerActionTitle"), getPendingIntent(notificationID, "callAnswer",json))
-                .addAction(0, json.getString("declineActionTitle"), callDismissIntent)
+                .addAction(0, json.getString("declineActionTitle"), getPendingIntent(notificationID, "callDismiss",json))
                 .build();
 
         NotificationManager notificationManager = notificationManager();
